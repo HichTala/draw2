65d7ee49bf0bd6707251898015ae4627c1f96423
Hich Tala <98521878+HichTala@users.noreply.github.com>
2025-05-07 17:39:43 +0200

    Work in progress in main module and draw class init function clean up

---
+++ draw/__main__.py
@@ -0,0 +1,117 @@
+import argparse
+import os
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from draw.draw import Draw
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser('Detecting and Recognizing a Wide Range of Yu-Gi-Oh! Cards', add_help=True)
+
+    parser.add_argument('--source', type=str,
+                        help="Source to use for processing refer to github.com/HichTala/draw2 for more details")
+
+    parser.add_argument("--save", nargs="?", const="output",
+                        help="Save the video. Optionally provide a path. If not, saves as 'output' in current dir.")
+
+    parser.add_argument('--show', action='store_true',
+                        help="Show video.")
+    parser.add_argument('--display-card', action='store_true',
+                        help="Display one of the cards that appear at a time.")
+
+    parser.add_argument('--deck-list', default=None, type=str,
+                        help="Path or link to the deck list.")
+
+    parser.add_argument('--fps', default=60, type=int,
+                        help="FPS of the saved video.")
+
+    return parser.parse_args()
+
+
+def show(outputs):
+    image = Image.fromarray(np.asarray(outputs['image'])[..., ::-1])
+    image.show(title="draw2 - Stream")
+
+
+def save(is_image, outputs, video_writer, save_path):
+    if is_image:
+        cv2.imwrite(f"{save_path}.png", outputs['image'])
+    else:
+        video_writer.write(outputs['image'])
+
+
+def display_card(outputs, counts, displayed):
+    for label in outputs['predictions']:
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+
+    for label, count in counts.items():
+        if count > 60:
+            if label not in displayed:
+                displayed[label] = 6
+                path = os.path.join("dataset", label)
+                filename = os.listdir(path)[0]
+
+                image = Image.open(os.path.join(path, filename))
+                image.show(title="draw2 - Card")
+            else:
+                displayed[label] -= 1
+                if displayed[label] == 0:
+                    del displayed[label]
+                    counts[label] = 0
+
+
+def main(args):
+    draw = Draw(
+        config="./configs/config.json",
+        source=args.source,
+        deck_list=args.deck_list,
+        debug=False
+    )
+
+    is_image = False
+    image_types = ["bmp", "dng", "HEIC", "jpeg", "jpg", "mpo", "pfm", "png", "tif", "tiff", "webp"]
+    if args.save and args.source is not None and args.source.split('.')[-1] in image_types:
+        is_image = True
+        save_path = args.save if args.save.split(".")[-1] in image_types else "output.png"
+
+    if args.save and not is_image:
+        video_types = ["asf", "avi", "gif", "m4v", "mov", "mp4", "mpeg", "mpg", "ts", "wmv", "webm"]
+        first_frame = next(iter(draw.results)).orig_img
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        save_path = ".".join(args.save.split('.')[:-1]) if args.save.split(".")[-1] in video_types else "output"
+
+        video_writer = cv2.VideoWriter(f'{save_path}.avi', fourcc, args.fps,
+                                       (first_frame.shape[0], first_frame.shape[1]))
+
+    if args.display_card:
+        # TODO put those as arguments in side draw class
+        counts = {}
+        displayed = {}
+
+    try:
+        for result in draw.results:
+            outputs = draw.process(result, show=args.show)
+
+            if args.show:
+                show(outputs)
+
+            if args.save:
+                save(is_image, outputs, video_writer, save_path)
+
+            if args.display_card:
+                display_card(outputs, counts, displayed)
+
+        if args.save and not is_image:
+            video_writer.release()
+    except KeyboardInterrupt:
+        video_writer.release()
+
+
+if __name__ == '__main__':
+    args = parse_command_line()
+    main(args)
---
+++ draw/draw.py
@@ -0,0 +1,149 @@
+import json
+import os
+
+import cv2
+import numpy as np
+import requests
+import torch
+from PIL import Image
+from transformers import AutoImageProcessor, pipeline
+from ultralytics import YOLO
+
+from draw import utils
+
+
+def download_file(url, destination):
+    response = requests.get(url)
+    with open(destination, 'wb') as file:
+        file.write(response.content)
+
+
+class Draw:
+    def __init__(self, config, source, deck_list=None, debug=False):
+        with open(config, "rb") as f:
+            self.configs = json.load(f)
+        with open(deck_list) as f:
+            self.deck_list = [line.rstrip() for line in f.readlines()]
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        if not os.path.isfile(self.configs["yolo_path"]):
+            download_file(self.configs["hf_yolo"], self.configs["yolo_path"])
+
+        model_regression = YOLO(self.configs["yolo_path"])
+        self.results = model_regression.track(
+            source=source,
+            show_labels=False,
+            save=False,
+            device=device,
+            stream=True,
+            verbose=False
+        )
+
+        image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
+        self.classifier = pipeline(
+            "image-classification",
+            model="HichTala/draw",
+            image_processor=image_processor,
+            device_map=device
+        )
+
+        self.debug_mode = debug
+
+    def process(self, result, show=False, display=False):
+        outputs = {}
+
+        if show:
+            outputs['predictions'] = []
+        if display:
+            outputs['image'] = result.orig_img.copy()
+
+        for nbox, boxe in enumerate(result.obb.xyxyxyxyn):
+            x1, y1, _, _, x2, y2, _, _ = boxe.cpu().numpy().astype(int)
+
+            max_aspect_ratio = self.configs['max_aspect_ratio']
+            min_aspect_ratio = self.configs['min_aspect_ratio']
+            aspect_ratio = max(
+                result.obb.xywhr[nbox][2], result.obb.xywhr[nbox][3]
+            ) / min(
+                result.obb.xywhr[nbox][2], result.obb.xywhr[nbox][3]
+            )
+
+            if self.debug_mode and show:
+                cv2.putText(outputs['image'], "aspect ratio :" + str(aspect_ratio), (x1, y2),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            1.0,
+                            (255, 255, 255),
+                            2)
+
+            if max_aspect_ratio > aspect_ratio > min_aspect_ratio:
+                boxe = np.float32(
+                    [[b[0] * outputs['image'].shape[1], b[1] * outputs['image'].shape[0]] for b in boxe.cpu()]
+                )
+                output_pts = np.float32([
+                    [224, 224],
+                    [224, 0],
+                    [0, 0],
+                    [0, 224]
+                ])
+                perspective_transform = cv2.getPerspectiveTransform(boxe, output_pts)
+                roi = cv2.warpPerspective(
+                    outputs['image'], perspective_transform, (224, 224), flags=cv2.INTER_LINEAR
+                )
+                contours = utils.extract_contours(roi)
+
+                if contours != ():
+                    contour = contours[np.array(list(map(cv2.contourArea, contours))).argmax()]
+                    box_txt, txt_aspect_ratio = utils.get_txt(contour)
+
+                    max_txt_aspect_ratio = self.configs["max_txt_aspect_ratio"]
+                    min_txt_aspect_ratio = self.configs["min_txt_aspect_ratio"]
+                    max_txt_area = self.configs["max_txt_area"]
+                    min_txt_area = self.configs["min_txt_area"]
+
+                    text_area = cv2.contourArea(box_txt)
+
+                    if self.debug_mode and show:
+                        cv2.putText(outputs['image'],
+                                    f"text area : {str(text_area)}",
+                                    (x1, y1 + (y2 - y1) // 2),
+                                    cv2.FONT_HERSHEY_PLAIN,
+                                    1.0,
+                                    (255, 255, 255),
+                                    2)
+                        cv2.putText(outputs['image'],
+                                    f"text aspect ratio : {str(txt_aspect_ratio)}",
+                                    (x1, y1 + (y2 - y1) // 4),
+                                    cv2.FONT_HERSHEY_PLAIN,
+                                    1.0,
+                                    (255, 255, 255),
+                                    2)
+
+                    if (
+                            (min_txt_aspect_ratio < txt_aspect_ratio < max_txt_aspect_ratio)
+                            and
+                            (min_txt_area < text_area < max_txt_area)
+                    ):
+                        rotation = utils.get_rotation(boxes=result.obb.xywhr[nbox], box_txt=box_txt)
+                        if rotation is None:
+                            break
+
+                        if rotation != 0:
+                            roi = cv2.rotate(roi, rotation)
+
+                        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+                        roi = Image.fromarray(roi)
+
+                        output = self.classifier(roi)
+
+                        for label in output[:]['label']:
+                            if label.split('-')[-1] in self.deck_list:
+                                outputs['predictions'].append(label)
+                                cv2.putText(outputs['image'], ' '.join(label.split('-')[:-2]), (x1, y1),
+                                            cv2.FONT_HERSHEY_PLAIN,
+                                            1.0,
+                                            (255, 255, 255),
+                                            2)
+                                break
+                        cv2.rectangle(outputs['image'], (x1, y1), (x2, y2), color=(255, 152, 119), thickness=2)
+        return outputs
