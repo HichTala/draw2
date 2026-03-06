efebcc5028e78633880f9a7d8af9d04eccc6a7b5
HichTala <98521878+HichTala@users.noreply.github.com>
2026-03-06 16:29:33 +0100

    Enhance shared memory handling with numpy support and logging for Windows

--- src/draw/run.py
+++ src/draw/run.py
@@ -5,6 +5,7 @@ from collections import deque
 from multiprocessing import shared_memory
 import mmap
 
+import numpy as np
 from draw.draw import Draw
 from draw.utils import read_shared_frame, get_deck_list
 
@@ -12,33 +13,17 @@ import os
 import sys
 import logging
 
-log_dir = os.path.join(os.environ["APPDATA"], "obs-studio")
-os.makedirs(log_dir, exist_ok=True)
-
-log_path = os.path.join(log_dir, "python_subprocess.log")
-
-# --- logging (flushes immediately) ---
-logging.basicConfig(
-    filename=log_path,
-    level=logging.DEBUG,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    force=True,   # important if logging was configured before
-)
-
-# --- redirect print() too ---
-log_file = open(log_path, "a", buffering=1)  # line-buffered
-sys.stdout = log_file
-sys.stderr = log_file
-
-logging.info("Python subprocess started")
-print("print() is real-time now")
-
 OBS_SHM_NAME = "obs_shared_memory"
 PYTHON_SHM_NAME = "python_shared_memory"
 HEADER_FORMAT = "II"
 HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 
-SIZE = 8104 * 1024
+MAX_FRAME_WIDTH = 3840
+MAX_FRAME_HEIGHT = 2160
+BYTES_PER_PIXEL = 4  # RGBA8
+FRAME_BUFFER_SIZE = MAX_FRAME_WIDTH * MAX_FRAME_HEIGHT * BYTES_PER_PIXEL
+
+SIZE = HEADER_SIZE + FRAME_BUFFER_SIZE
 
 
 class DrawSharedMemoryHandler:
@@ -48,6 +33,7 @@ class DrawSharedMemoryHandler:
         self.obs_shm = None
         self.python_shm = None
         self.buf = None
+        self.shm_array = None
 
         self.minimum_out_of_screen_time = minimum_out_of_screen_time
         self.minimum_screen_time = minimum_screen_time
@@ -69,8 +55,12 @@ class DrawSharedMemoryHandler:
         while continue_execution:
             continue_execution = True if address is None else address.contents.value
             try:
-                self.obs_shm = mmap.mmap(-1, SIZE, tagname=OBS_SHM_NAME, access=mmap.ACCESS_READ)
-                print("mapped size:", self.obs_shm.size())
+                if sys.platform == 'win32':
+                    self.obs_shm = mmap.mmap(-1, SIZE, tagname=OBS_SHM_NAME, access=mmap.ACCESS_READ)
+                    print("mapped size:", self.obs_shm.size())
+                else:
+                    self.obs_shm = shared_memory.SharedMemory(name=OBS_SHM_NAME)
+                    print("mapped size:", self.obs_shm.size)
             except FileNotFoundError:
                 continue
             except ValueError:
@@ -82,22 +72,22 @@ class DrawSharedMemoryHandler:
             break
 
         print(f"Shared memory found")
+        buf = memoryview(self.obs_shm)
         while continue_execution:
             try:
                 continue_execution = True if address is None else address.contents.value
-                self.obs_shm.seek(0)
-                buf = self.obs_shm.read(SIZE) #self.obs_shm.size())
                 image = read_shared_frame(buf, HEADER_SIZE, HEADER_FORMAT)
             except TypeError as e:
                 print("type error: ", e)
-                breakpoint()
+                break
             except ValueError as e:
                 print("value error: ", e)
-                breakpoint()
+                break
             except KeyboardInterrupt:
                 print("Stopping Draw2...")
                 self.obs_shm.close()
                 self.python_shm.close()
+                log_file.close()
                 return
 
             if image.size[0] == 0 or image.size[1] == 0:
@@ -115,13 +105,12 @@ class DrawSharedMemoryHandler:
                 for result in results:
                     outputs = self.draw.process(result)
                     self.display_card(outputs)
-            # except Exception as e:
-            #     print(f"Error processing shared memory: {e}")
-            #     breakpoint()
+
             except KeyboardInterrupt:
                 print("Stopping Draw2...")
                 self.obs_shm.close()
                 self.python_shm.close()
+                log_file.close()
                 return
 
     def display_card(self, outputs):
@@ -154,26 +143,33 @@ class DrawSharedMemoryHandler:
                         self.counts[label] = 0
 
     def send_image_to_obs(self, image):
-        img_rgba = image.convert("RGBA")
-        width, height = img_rgba.size
-        img_bytes = img_rgba.tobytes()
+        img_array = np.array(image.convert("RGBA"))
+        width, height = img_array.shape
 
-        total_size = HEADER_SIZE + len(img_bytes)
+        total_size = HEADER_SIZE + img_array.nbytes
 
         if self.python_shm is None:
-            self.python_shm = mmap.mmap(-1, total_size, tagname=PYTHON_SHM_NAME, access=mmap.ACCESS_WRITE)
-            self.python_shm.seek(0)
-            self.python_shm.write(b"\x00" * total_size)
+            if sys.platform == 'win32':
+                self.python_shm = mmap.mmap(-1, total_size, tagname=PYTHON_SHM_NAME, access=mmap.ACCESS_WRITE)
+                self.python_shm.seek(0)
+                self.python_shm.write(b"\x00" * total_size)
+            else:
+                self.python_shm = shared_memory.SharedMemory(name=PYTHON_SHM_NAME, create=True, size=total_size)
 
-        if total_size != self.python_shm.size:
-            self.python_shm.close()
-            self.python_shm = mmap.mmap(-1, total_size, tagname=PYTHON_SHM_NAME, access=mmap.ACCESS_WRITE)
-            self.python_shm.seek(0)
-            self.python_shm.write(b"\x00" * total_size)
+        if sys.platform == 'win32' and total_size != self.python_shm.size():
+                self.python_shm.close()
+                self.python_shm = mmap.mmap(-1, total_size, tagname=PYTHON_SHM_NAME, access=mmap.ACCESS_WRITE)
+                self.python_shm.seek(0)
+                self.python_shm.write(b"\x00" * total_size)
+                self.shm_array = np.ndarray((height, width, 4), dtype=np.uint8, buffer=self.buf)
+        elif total_size != self.python_shm.size:
+                self.python_shm.close()
+                self.python_shm.unlink()
+                self.python_shm = shared_memory.SharedMemory(name=PYTHON_SHM_NAME, create=True, size=total_size)
+                self.shm_array = np.ndarray((height, width, 4), dtype=np.uint8, buffer=self.buf)
 
-        self.python_shm.seek(0)
-        self.python_shm.write(struct.pack(HEADER_FORMAT, width, height))
-        self.python_shm.write(img_bytes)
+
+        self.shm_array[:] = img_array
 
         print(f"Sent image {width}x{height} to OBS")
 
@@ -211,5 +207,27 @@ def run(
 
 
 if __name__ == '__main__':
+    if sys.platform == 'win32':
+        log_dir = os.path.join(os.environ["APPDATA"], "obs-studio")
+        os.makedirs(log_dir, exist_ok=True)
+
+        log_path = os.path.join(log_dir, "python_subprocess.log")
+
+        # --- logging (flushes immediately) ---
+        logging.basicConfig(
+            filename=log_path,
+            level=logging.DEBUG,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            force=True,  # important if logging was configured before
+        )
+
+        # --- redirect print() too ---
+        log_file = open(log_path, "a", buffering=1)  # line-buffered
+        sys.stdout = log_file
+        sys.stderr = log_file
+
+        logging.info("Python subprocess started")
+        print("print() is real-time now")
+
     print("run")
     run()
